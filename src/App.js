d1b41a5d6642991ca08c2cdf8de3ffcf362846f8
mirko <autieromir@gmail.com>
2022-11-25 11:23:35 +0100

    components_4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React from "react";
-import {Hello}  from "./Hello";
 
 export class App extends React.Component{
     render() {
-        return(
-        <div>
-            <Hello />
-        </div>
-        )
+        return
     }
 }
