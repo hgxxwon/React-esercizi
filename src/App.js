e428ec1c409ab91cf093b4260527951dbc9e877a
mirko <autieromir@gmail.com>
2022-11-29 18:22:49 +0100

    conditional_rendering_5

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export class App extends React.Component{
     render() {
         return(
             <div>
-                <Welcome name="John" age={26}/>
+                <Welcome name="Mirko" age={17}/>
             </div>
             ) 
     }
