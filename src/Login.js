c7b56d4381795f4436ba92ad7d8653e07d16ef19
mirko <autieromir@gmail.com>
2022-12-01 03:03:46 +0100

    forms_6

--- src/Login.js
+++ src/Login.js
@@ -3,17 +3,13 @@ import React from "react";
 export class Login extends React.Component{
    handleForm = (event) => {
     event.preventDefault()
-
-    const username = event.target.elements.username.value
-    const password = event.target.elements.password.value
-    const checked = event.target.elements.remember.checked
    } 
     render(){
         return (
             <div>
                 <h2>A Simple Form</h2>
                 <form onSubmit={this.handleForm}>
-                    <input name="username" />
+                    <input autoFocus name="username" /> {/*added autofocus*/}
                     <input name="password" type="password" />
                     <input name="remember" type="checkbox"/>
                     <button type="submit">Submit</button>
