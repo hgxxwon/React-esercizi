06194616909b92434888ed83fd2f2667247222fa
mirko <autieromir@gmail.com>
2022-12-02 00:14:39 +0100

    component_composition_1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
-import { Login } from "./Login";
+import { Container } from "./Container";
 
 export class App extends React.Component{
     render() {
         return(
-            <div>
-                <Login/>
-            </div>
+                <Container children="Prova">
+                </Container>
             ) 
     }
 }
