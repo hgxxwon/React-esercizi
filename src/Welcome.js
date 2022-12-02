b0b2dd34a79b41cfa9edf801f7515807bc4325db
mirko <autieromir@gmail.com>
2022-12-02 23:20:48 +0100

    function_components_1

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,26 +1,8 @@
 import React from "react";
-import { LanguageContext } from "./LanguageContext";
-
-const Strings = {
-    en: {
-        CURRENT_TITLE: "You chosed English"
-    },
-
-    it: {
-        CURRENT_TITLE: "Hai scelto l'Italiano"
-    }
-}
-
-export class Welcome extends React.Component{
-    render(){
-        return(
-            <div>
-                <LanguageContext.Consumer>
-                    {(language) => {
-                        return <div><h1>{Strings[language].CURRENT_TITLE}</h1></div>
-                    }}
-                </LanguageContext.Consumer>
-            </div>
-        )
-    }
+export function Welcome({name = "Mirko"}){
+    return (
+        <div>
+            <h1>Hello, {name}</h1>
+        </div>
+    )
 }
