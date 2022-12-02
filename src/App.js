1e9c87ed2485d0e9830ea4da4656980389ff898e
mirko <autieromir@gmail.com>
2022-12-02 23:41:06 +0100

    function_components_2

--- src/App.js
+++ src/App.js
@@ -4,21 +4,12 @@ import { Sum } from "./Sum";
 import { Welcome } from "./Welcome";
 
 export class App extends React.Component{
-    state = {
-        language: 'en'
-    }
-
-    handleLanguage = (event) => {
-        this.setState({
-            language: event.target.value
-        })
-    }
     render() {
         return(
             <div>
                 <Container title="Benvenuto">
                     <Welcome />
-                    <Sum number1={9} number2={3} />
+                    <Sum numbers={[1,5,9]} />
                 </Container>
             </div>
             ) 
--- src/Sum.js
+++ src/Sum.js
@@ -3,7 +3,7 @@ import React from "react";
 export function Sum(props){
     return (
         <div>
-            <h1>{[props.number1 + props.number2]}</h1>
+            <h1>{props.numbers.reduce((a,b)=> a + b, 0)}</h1>
         </div>
     )
 }
