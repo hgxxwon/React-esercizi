cd6bb05960960203130822f8934c72b7c5b538be
mirko <autieromir@gmail.com>
2022-12-03 01:19:27 +0100

    usestate_2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { ClickCounter } from "./ClickCounter";
 import { Container } from "./Container";
+import { Login } from "./Login";
 import { Welcome } from "./Welcome";
 
 export class App extends React.Component{
@@ -10,6 +11,7 @@ export class App extends React.Component{
                 <Container title="Benvenuto">
                     <Welcome />
                     <ClickCounter />
+                    <Login />
                 </Container>
             </div>
             ) 
