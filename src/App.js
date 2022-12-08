e3b15db1abe71686266ba55add445d32451ed2ae
mirko <autieromir@gmail.com>
2022-12-08 11:26:41 +0100

    forms 1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-import { Link, Route, Routes } from "react-router-dom"
-import { Container } from "./Container";
-import { Welcome } from "./Welcome";
-import { Counter } from "./Counter"
-import { ShowGithubuser } from "./ShowGithubUser"
-
-
+import { InteractiveWelcome } from "./InteractiveWelcome";
 
 export function App(){
         return(
-                <Container>
-                    <div>
-                        <Link to="/">Home</Link> | <Link to="counter">Counter</Link> | <Link to="users/:username">Username</Link>
-                    </div>
-                    <Routes>
-                        <Route path="/" element={<Welcome name="Jimmy"/>} />
-                        <Route path="counter" element={<Counter/>} />
-                        <Route path="users/:username" element={<ShowGithubuser/>}/>
-                    </Routes>
-                </Container>
+                <div>
+                    <InteractiveWelcome />
+                </div>
             ) 
     }
