07140121714fc5f01fd6fac57a4c74723a37b226
mirko <autieromir@gmail.com>
2022-12-08 09:59:06 +0100

    react router 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import { Link, Route, Routes } from "react-router-dom"
 import { Container } from "./Container";
 import { Welcome } from "./Welcome";
-import { Route, Routes } from "react-router-dom"
+import { Counter } from "./Counter"
+
 
 
 export function App(){
         return(
                 <Container>
+                    <div>
+                        <Link to="/">Home</Link> | <Link to="counter">Counter</Link>
+                    </div>
                     <Routes>
                         <Route path="/" element={<Welcome name="Jimmy"/>} />
+                        <Route path="counter" element={<Counter/>} />
                     </Routes>
                 </Container>
             ) 
