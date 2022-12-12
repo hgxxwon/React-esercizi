f242a525cd600f034a09c3f1160f782c635227ae
mirko <autieromir@gmail.com>
2022-12-12 22:31:21 +0100

    useEffect_4

--- src/App.js
+++ src/App.js
@@ -4,19 +4,20 @@ import { Counter } from "./Counter";
 import { Welcome } from "./Welcome";
 import { Container } from "./Container";
 import { ShowGithubuser } from "./ShowGithubUser";
-import { GitHubUser } from "./GitHubUser";
+import { GithubUserList } from "./GithubUserList";
 
 export function App(){
         return(
             <Container>
-            <div>
-                <Link to="/">Home</Link> | <Link to="counter">Counter</Link>
+            {/* <div>
+                <Link to="/">Home</Link> | <Link to="counter">Counter</Link> | <Link to="/:username">Username</Link>
             </div>
             <Routes>
                 <Route path="/" element={<Welcome name="Jimmy"/>} />
                 <Route path="counter" element={<Counter/>} />
                 <Route path="/:username" element={<ShowGithubuser />}/>
-            </Routes>
+            </Routes> */}
+            <GithubUserList />
         </Container>
             ) 
     }
