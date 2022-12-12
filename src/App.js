ebc0e55ca1e224e4c972bb3536c2132932139a7c
mirko <autieromir@gmail.com>
2022-12-12 22:04:54 +0100

    react router 3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import { FilteredList } from "./FilteredList";
+import { Route, Routes } from "react-router";
+import { Link } from "react-router-dom";
+import { Counter } from "./Counter";
+import { Welcome } from "./Welcome";
+import { Container } from "./Container";
+import { ShowGithubuser } from "./ShowGithubUser";
+import { GitHubUser } from "./GitHubUser";
 
 export function App(){
         return(
-                <div>
-                    <FilteredList />
-                </div>
+            <Container>
+            <div>
+                <Link to="/">Home</Link> | <Link to="counter">Counter</Link>
+            </div>
+            <Routes>
+                <Route path="/" element={<Welcome name="Jimmy"/>} />
+                <Route path="counter" element={<Counter/>} />
+                <Route path="/:username" element={<ShowGithubuser />}/>
+            </Routes>
+        </Container>
             ) 
     }
